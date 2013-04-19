74904150ade9d61c681582dd292a306b1f8fb959
Ken Taylor <taylor.kenneth@gmail.com>
2013-04-18 22:10:57 -0400

    Initial commit

---
+++ lib/duke-dedupe.rb
@@ -0,0 +1,7 @@
+require "duke-dedupe/version"
+
+module Duke
+  module Dedupe
+    # Your code goes here...
+  end
+end
---
+++ lib/duke-dedupe/version.rb
@@ -0,0 +1,5 @@
+module Duke
+  module Dedupe
+    VERSION = "0.0.1"
+  end
+end
